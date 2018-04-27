876714d5ef978d56236c784a05c59448952457fb
Cameron <cameron.farzaneh@gmail.com>
2018-04-27 11:33:04 -0400

    update

--- protein.py
+++ protein.py
@@ -18,8 +18,7 @@ for line in fin:
 		coord_list = np.concatenate([coord_list, j])
 
 	coord_list = np.reshape(coord_list,(-1,4))
-	# print(coord_list)
-	break #For now we're going to read only the first protein
+	print(coord_list)
 
 nrows = coord_list.shape[0]
 
@@ -49,25 +48,22 @@ for x in range(0,Grid_X_size):
 
 				#Channel 0->20, funcyional atom 1-21
 
-# # 5d = number of samples
-# model = Sequential()
-# # I think 21 should be nrows
-# # 5th mini batch size
-# model.add(Conv3D(21, kernel_size=(5, 5, 5, 5), strides=(1, 1, 1, 1),
-#                  activation='relu',
-#                  input_shape=(Grid_X_size,Grid_Y_size,Grid_Z_size,21)))
-# model.add(MaxPooling3D(pool_size=(2, 2, 2, 2), strides=(1, 1, 1, 1)))
-# model.add(Conv3D(21, (3, 3, 3), activation='relu')) # Need to fix this
-# model.add(MaxPooling3D(pool_size=(2, 2, 2))) 
-# model.add(Flatten())
-# model.add(Dense(100, activation='relu'))
-# model.add(Dense(20, activation='softmax'))
-# # we have 20 classify
-# #loss cross entopy!, 100 epochs
+model = Sequential()
+model.add(Conv3D(21, kernel_size=(5, 5, 5), strides=(1, 1, 1),
+                 activation='relu',
+                 input_shape=(Grid_X_size,Grid_Y_size,Grid_Z_size,21)))
+model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(1, 1, 1)))
+model.add(Conv3D(21, (3, 3, 3), strides=(1, 1, 1), activation='relu')) # Need to fix this
+model.add(MaxPooling3D(pool_size=(2, 2, 2))) 
+model.add(Flatten())
+model.add(Dense(100, activation='relu'))
+model.add(Dense(20, activation='softmax'))
 
-# model.fit(tensor, y_train,
-#           batch_size=10,
-#           epochs=10,
-#           verbose=1,
-#           validation_data=(x_test, y_test),
-#           callbacks=[history])
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adam(lr=0.01),
+              metrics=['accuracy'])
+
+model.fit(tensor, y=None,
+          batch_size=10,
+          epochs=100,
+          verbose=1)
