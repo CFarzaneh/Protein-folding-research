afac7fc585421071f328de0681d4b978df41f197
Cameron <cameron.farzaneh@gmail.com>
2018-04-27 16:19:37 -0400

    constructing 5d tensor

--- protein.py
+++ protein.py
@@ -7,14 +7,24 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 
 fin = open("pdb1a1x.txt", 'r')
-proteinLabels = set()
+
+# Hyperparameters
+Window_width = 2
+Grid_cell_size = 1
+Grid_X_size = 21
+Grid_Y_size = 21
+Grid_Z_size = 21
+Coord_X_min = -10
+Coord_Y_min = -10
+Coord_Z_min = -10
+
+batchOfTensors = np.array([])
 
 # Read in the data
 for line in fin:
 	line = line.strip().split('\t')
 	protein = line.pop(0)
-	#print(protein)
-	proteinLabels.add(protein)
+	print(protein)
 
 	coord_list = np.array([])
 	for i in line:
@@ -24,60 +34,41 @@ for line in fin:
 	coord_list = np.reshape(coord_list,(-1,4))
 	#print(coord_list)
 
-# Build the Labels, One hot vector.
-print(proteinLabels)
-proteinLabels = list(proteinLabels)
-proteinLabels = np.array(proteinLabels)
-protein_label_encoder = pd.factorize(proteinLabels)
-encoder = OneHotEncoder()
-protein_labels_1hot = encoder.fit_transform(protein_label_encoder[0].reshape(-1,1))
-onehot_array = protein_labels_1hot.toarray()
-print(onehot_array)
+	nrows = coord_list.shape[0]
 
-# nrows = coord_list.shape[0]
+	tensor = np.zeros((Grid_X_size,Grid_Y_size,Grid_Z_size,21)) #4D-Tensor
 
-# # Hyperparameters
-# Window_width = 2
-# Grid_cell_size = 1
-# Grid_X_size = 21
-# Grid_Y_size = 21
-# Grid_Z_size = 21
-# Coord_X_min = -10
-# Coord_Y_min = -10
-# Coord_Z_min = -10
+	for x in range(0,Grid_X_size):
+		Xx = (x*Grid_cell_size)+Coord_X_min
+		for y in range(0,Grid_Y_size):
+			Yy = (y*Grid_Y_size)+Coord_Y_min
+			for z in range(0,Grid_Z_size):
+				Zz = (z*Grid_cell_size)+Coord_Z_min
+				for m in range(0,nrows):
+					Channel = int(coord_list[m][0])
+					DistanceSq = ((Xx - float(coord_list[m][1]))**2)+((Yy - float(coord_list[m][2]))**2)+((Zz - float(coord_list[m][3]))**2)
+					Contribution = np.exp(-DistanceSq/(2*(Window_width**2)))
+					tensor[x][y][z][Channel] += Contribution
 
-# tensor = np.zeros((Grid_X_size,Grid_Y_size,Grid_Z_size,21)) #4D-Tensor
-
-# for x in range(0,Grid_X_size):
-# 	Xx = (x*Grid_cell_size)+Coord_X_min
-# 	for y in range(0,Grid_Y_size):
-# 		Yy = (y*Grid_Y_size)+Coord_Y_min
-# 		for z in range(0,Grid_Z_size):
-# 			Zz = (z*Grid_cell_size)+Coord_Z_min
-# 			for m in range(0,nrows):
-# 				Channel = int(coord_list[m][0])
-# 				DistanceSq = ((Xx - float(coord_list[m][1]))**2)+((Yy - float(coord_list[m][2]))**2)+((Zz - float(coord_list[m][3]))**2)
-# 				Contribution = np.exp(-DistanceSq/(2*(Window_width**2)))
-# 				tensor[x][y][z][Channel] += Contribution
-
-# 				#Channel 0->20, funcyional atom 1-21
+	batchOfTensors.push(tensor)
+	print(tensor.shape)
 
 # model = Sequential()
-# model.add(Conv3D(21, kernel_size=(5, 5, 5), strides=(1, 1, 1),
-#                  activation='relu',
-#                  input_shape=(Grid_X_size,Grid_Y_size,Grid_Z_size,21)))
+# model.add(Conv3D(21, kernel_size=(5, 5, 5), strides=(1, 1, 1), activation='relu', input_shape=(Grid_X_size,Grid_Y_size,Grid_Z_size,21)))
 # model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(1, 1, 1)))
-# model.add(Conv3D(21, (3, 3, 3), strides=(1, 1, 1), activation='relu')) # Need to fix this
-# model.add(MaxPooling3D(pool_size=(2, 2, 2))) 
+# model.add(Conv3D(21, kernel_size=(3, 3, 3), strides=(1, 1, 1), activation='relu'))
+# model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(1, 1, 1))) 
 # model.add(Flatten())
-# model.add(Dense(100, activation='relu'))
+# model.add(Dense(100, activation='relu')) ####
 # model.add(Dense(20, activation='softmax'))
 
 # model.compile(loss=keras.losses.categorical_crossentropy,
 #               optimizer=keras.optimizers.Adam(lr=0.01),
 #               metrics=['accuracy'])
 
-# model.fit(tensor, y=None,
-#           batch_size=10,
-#           epochs=100,
-#           verbose=1)
+# model.fit(x_train, y_train,
+#           batch_size=batch_size,
+#           epochs=epochs,
+#           verbose=1,
+#           validation_data=(x_test, y_test),
+#           callbacks=[history])
