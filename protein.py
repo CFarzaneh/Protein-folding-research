04c9869f61203f52cfddcae79f61b2b72ecac6d1
Cameron <cameron.farzaneh@gmail.com>
2018-05-14 11:06:37 -0400

    added more inputFiles

--- protein.py
+++ protein.py
@@ -100,9 +100,9 @@ print(onehot_array.shape)
 input_shape = input_data[0].shape
 
 model = Sequential()
-model.add(Conv3D(32, kernel_size=(5, 5, 5), strides=(1, 1, 1), activation='relu', input_shape=input_shape))
-model.add(MaxPooling3D(pool_size=(4, 4, 4), strides=(1, 1, 1)))
-model.add(Conv3D(64, kernel_size=(3, 3, 3), strides=(1, 1, 1), activation='relu'))
+model.add(Conv3D(22, kernel_size=(1, 1, 1), strides=(1, 1, 1), activation='relu', input_shape=input_shape))
+model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(1, 1, 1)))
+model.add(Conv3D(22, kernel_size=(1, 1, 1), strides=(1, 1, 1), activation='relu'))
 model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(1, 1, 1))) 
 
 model.add(Flatten())
