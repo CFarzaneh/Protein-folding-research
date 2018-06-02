e97ca32d562a3deb3fd7d615e9e147ffda755f19
Cameron <cameron.farzaneh@gmail.com>
2018-06-02 14:23:59 -0400

    work

--- embedding/embedding_demo.py
+++ embedding/embedding_demo.py
@@ -6,6 +6,7 @@ from keras.models import Model
 from keras.callbacks import TensorBoard
 import keras
 from time import time
+from tqdm import tqdm
 
 # Just checking on a single file 
 # For all the 1000 file, you can use each file as a batch
@@ -17,17 +18,22 @@ filelist = os.listdir(dataPath)
 data = []
 label = []
 
-for i in filelist:
-    print(i)
+print("Loading data")
+j = 0
+numOfFilesToInput = 100 #Number of files to load at once
+for i in tqdm(filelist, total=numOfFilesToInput):
     data.append(np.load(dataPath+i))
     fileName = i.split('_')[0]
     label.append(np.load(labelPath+fileName+"_label.npy"))
+    if j == numOfFilesToInput:
+      break
+    j+=1
 
 data = np.concatenate(data, axis=0)
 label = np.concatenate(label, axis=0)
 
-print(data.shape)
-print(label.shape)
+#print(data.shape)
+#print(label.shape)
 
 # Change label to one-hot encoding
 # For all the file, you need to club all the labels files together and then change into one-hot encoding for sync
@@ -37,10 +43,9 @@ encoder = OneHotEncoder()
 protein_labels_1hot = encoder.fit_transform(protein_label_encoder[0].reshape(-1,1))
 onehot_array = protein_labels_1hot.toarray()
 
-print(onehot_array.shape)
-
 data1 = data.reshape((-1, 21, 19, 19, 19, 1))
 print(data1.shape)
+print(onehot_array.shape)
 
 # For parallel 21 computations, Create 21 list to insert the model
 data2 = [[] for _ in range(21)]
@@ -50,7 +55,6 @@ for sample in data1:
 data2 = [np.array(i) for i in data2]
 
 # Demo Architecture 
-#plot_losses = livelossplot.PlotLossesKeras()
 
 def parallel_computation(inputs):
     convs = []
@@ -63,18 +67,19 @@ inputs = [Input(shape=(19,19,19,1)) for _ in range(21)] #19x19x19
 adds = parallel_computation(inputs)
 
 conv1 = Conv3D(1,kernel_size=(3, 3, 3), strides=(1, 1, 1), activation='relu', input_shape=(19,19,19,3))(adds) #3x3x3
-pool1 = MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2))(conv1)
-'''
-conv2 = Conv3D(1,kernel_size=(1, 1, 1), strides=(1, 1, 1), activation='relu')(pool1) #3x3x3
-pool2 = MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2))(conv2)
+conv2 = Conv3D(1,kernel_size=(3, 3, 3), strides=(1, 1, 1), activation='relu')(conv1)
+pool1 = MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2))(conv2)
 
-conv3 = Conv3D(1,kernel_size=(1, 1, 1), strides=(1, 1, 1), activation='relu')(pool2) #3x3x3
-pool3 = MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2))(conv3)
-'''
+#conv2 = Conv3D(1,kernel_size=(1, 1, 1), strides=(1, 1, 1), activation='relu')(pool1) #3x3x3
+#pool2 = MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2))(conv2)
+
+#conv3 = Conv3D(1,kernel_size=(1, 1, 1), strides=(1, 1, 1), activation='relu')(pool2) #3x3x3
+#pool3 = MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2))(conv3)
+
 #another convolution layer, max pooling, another convolution layer 3x3x3
 
 flatten1 = Flatten()(pool1)
-dense1 = Dense(100, activation='relu')(flatten1)
+dense1 = Dense(200, activation='relu')(flatten1)
 out = Dense(20, activation='softmax')(dense1)
 model = Model(input= inputs,output = out)
 
@@ -88,7 +93,7 @@ model.compile(loss=keras.losses.categorical_crossentropy,
 tensorboard = TensorBoard(log_dir="logs/{}".format(time()))
 
 model.fit(data2, onehot_array,
-      batch_size=10,
+      batch_size=20,
       epochs=10,
       verbose=1,
       callbacks=[tensorboard],
